36b0d9b2177eb527463363052cc8fd811132e238
VamsiVajjhula <117925570+VamsiVajjhula-2022@users.noreply.github.com>
2023-07-03 07:34:34 +0530

    Add files via upload

---
+++ 4 b.py
@@ -0,0 +1,11 @@
+#Check:         E:\VS Trail and open with code and run py file with 
+#                      input Hello.txt
+
+#Below code is placed in the location
+# filename = input("Enter the filename: ")
+# with open(filename, 'r') as file:
+#     text = file.read()
+# words = text.split()
+# unique_words = sorted(set(words))
+# for word in unique_words:
+#     print(word)
